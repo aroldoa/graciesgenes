8918e2f8c8bc10eaf09bba0bbf6c8b672705fd3c
Aroldo Alvarez <aroldo@primomotif.com>
2014-02-21 17:18:33 -0600

    Completed Home Page HTML

---
+++ header.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<head>
+
+	<!-- Basic Page Needs
+  ================================================== -->
+	<meta charset="utf-8">
+	<title>Gracies Genes Boutique - Women's Vintage but Modern Clothing and Accessories</title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<!-- Mobile Specific Metas
+  ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<!-- CSS
+  ================================================== -->
+	<link rel="stylesheet" href="style.css">
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	<!-- Favicons
+	================================================== -->
+	<link rel="shortcut icon" href="images/favicon.ico">
+	<link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+
+
+
+</head>
+<body>
+<!-- Start Document
+================================================== -->
+
+<div class="upperdeck">
+	<div class="container16">
+		<div class="column6 gethelp">
+			Help Placing an Order?  <span><strong>210-222-2222</strong></span>
+		</div>
+		<div class="column10 upperdecklinks">
+			<ul id="toplinks">
+				<li><a href="#">My Account</a></li>
+				<li><a href="#">Create an Account</a></li>
+				<li><a href="#">Contact Us</a></li>
+				<li class="carticon"><a href="#">$0.00</a></li>
+			</ul>
+		</div>
+	</div>
+</div><!-- end of the upper top area -->
+<div class="container16 wrap shadow">
+	<header class="row">
+		<div class="column4 prefix6">
+			<a href="#"><img src="images/logo.png" alt="Gracies Genes"/></a>
+		</div>
+		<div class="column4 prefix2 search">
+			<form>
+				<input class="mainsearch" type="text" value="SEARCH HERE"/>
+				<input type="button" class="searchicon" value=""/>
+			</form>
+		</div>
+	</header><!-- end of header area -->
+	<nav class="row">
+		<ul>
+			<li><a href="#">New Arrivals</a></li>
+			<li class="drop"><a href="#">Clothing</a></li>
+			<li><a href="#">Jewelry</a></li>
+			<li class="drop"><a href="#">Baby Wear</a></li>
+			<li><a href="#">Gift Ideas</a></li>
+			<li><a href="#">On Sale</a></li>
+			<li class="last"><a href="#">Look Book</a></li>
+		</ul>
+	</nav><!-- end of main naviation -->
---
+++ index.php
@@ -0,0 +1,64 @@
+<?php include('header.php')?>
+	<div class="slideshow row">
+		<div class="column16">
+			<img src="images/slide.jpg" alt="Slideshow"/>
+		</div>
+	</div><!-- end of slideshow area -->
+	<div class="promobanner row">
+		<div class="column16">
+			<div class="title">FREE SHIPPING ON ALL ORDERS OVER $100</div>
+		</div>
+	</div><!-- end of promo banner -->
+	<div class="products-slider row">
+		<div class="column16">
+			<h2><span>Featured Products</span></h2>
+			<ul class="products">
+				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
+				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
+				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
+				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
+				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
+				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
+				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
+				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
+				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
+				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
+				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
+				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
+				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
+				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
+				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
+			</ul>
+		</div>
+	</div><!-- end of featured products -->
+	<div class="products-slider row">
+		<div class="column16">
+			<h2><span>Sale Items</span></h2>
+			<ul class="products">
+				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
+				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
+				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
+				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
+				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
+				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
+				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
+				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
+				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
+				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
+				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
+				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
+				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
+				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
+				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
+			</ul>
+		</div>
+	</div><!-- end of best sellers -->
+	<div class="promoarea row">
+		<div class="column8">
+			<img src="images/promo1.jpg" alt="promo1"/>
+		</div>
+		<div class="column8">
+			<img src="images/promo2.jpg" alt="promo2"/>
+		</div>
+	</div><!-- end of promo area -->
+<?php include('footer.php')?>
