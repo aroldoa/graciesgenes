3c52050bacb7356c4fea218fd2946d7089b50fc9
Aroldo Alvarez <aroldo@primomotif.com>
2014-02-23 17:26:47 -0600

    New Files
    Updated Category.php and Product.php

---
+++ category.php
@@ -0,0 +1,136 @@
+<?php include('header.php')?>
+<div class="product-pages">
+	<div class="sidebar">
+		<div class="column4">
+			<div class="widget">
+				<h2>Shop By</h2>
+				<ul id="category-listing">
+					<li><a href="#">Dresses</a></li>
+					<li><a href="#">Tops</a></li>
+					<li><a href="#">Bottoms</a></li>
+					<li><a href="#">Rompers & Jumpsuits</a></li>
+					<li><a href="#">Beauty</a></li>
+					<li><a href="#">Denim</a></li>
+					<li><a href="#">Swimwear</a></li>
+					<li><a href="#">Accessories</a></li>
+					<li><a href="#">Jewelry</a></li>
+					<li><a href="#">Sale</a></li>
+				</ul>
+			</div>
+
+			<div class="widget">
+				<h2>Size</h2>
+				<ul id="category-listing">
+					<li><a href="#">Dresses</a></li>
+					<li><a href="#">Tops</a></li>
+					<li><a href="#">Bottoms</a></li>
+					<li><a href="#">Rompers & Jumpsuits</a></li>
+					<li><a href="#">Beauty</a></li>
+					<li><a href="#">Denim</a></li>
+					<li><a href="#">Swimwear</a></li>
+					<li><a href="#">Accessories</a></li>
+					<li><a href="#">Jewelry</a></li>
+					<li><a href="#">Sale</a></li>
+				</ul>
+			</div>
+
+			<div class="widget">
+				<h2>Color</h2>
+				<ul id="category-listing">
+					<li><a href="#">Dresses</a></li>
+					<li><a href="#">Tops</a></li>
+					<li><a href="#">Bottoms</a></li>
+					<li><a href="#">Rompers & Jumpsuits</a></li>
+					<li><a href="#">Beauty</a></li>
+					<li><a href="#">Denim</a></li>
+					<li><a href="#">Swimwear</a></li>
+					<li><a href="#">Accessories</a></li>
+					<li><a href="#">Jewelry</a></li>
+					<li><a href="#">Sale</a></li>
+				</ul>
+			</div>
+
+
+		</div>
+	</div>
+	<div class="product-category">
+		<div class="column12 main">
+				<div class="category-banner">
+				<img src="images/category-banner.png" alt="banner"/>
+			</div>
+
+			<div class="product-listing">
+				<ul class="products-listing">
+				<li><a href="#"><img src="images/product.jpg" width="195px" alt=""/></a>
+					<h3>Product Title Here</h3>
+					<span class="price">$33</span>
+				</li>
+				<li><a href="#"><img src="images/product.jpg" width="195px" alt=""/></a>
+					<h3>Product Title Here</h3>
+					<span class="price">$33</span>
+				</li>
+				<li><a href="#"><img src="images/product.jpg" width="195px" alt=""/></a>
+					<h3>Product Title Here</h3>
+					<span class="price">$33</span>
+				</li>
+				<li><a href="#"><img src="images/product.jpg" width="195px" alt=""/></a>
+					<h3>Product Title Here</h3>
+					<span class="price">$33</span>
+				</li>
+				<li><a href="#"><img src="images/product.jpg" width="195px" alt=""/></a>
+					<h3>Product Title Here</h3>
+					<span class="price">$33</span>
+				</li>
+				<li><a href="#"><img src="images/product.jpg" width="195px" alt=""/></a>
+					<h3>Product Title Here</h3>
+					<span class="price">$33</span>
+				</li>
+				<li><a href="#"><img src="images/product.jpg" width="195px" alt=""/></a>
+					<h3>Product Title Here</h3>
+					<span class="price">$33</span>
+				</li>
+				<li><a href="#"><img src="images/product.jpg" width="195px" alt=""/></a>
+					<h3>Product Title Here</h3>
+					<span class="price">$33</span>
+				</li>
+				<li><a href="#"><img src="images/product.jpg" width="195px" alt=""/></a>
+					<h3>Product Title Here</h3>
+					<span class="price">$33</span>
+				</li>
+				<li><a href="#"><img src="images/product.jpg" width="195px" alt=""/></a>
+					<h3>Product Title Here</h3>
+					<span class="price">$33</span>
+				</li>
+				<li><a href="#"><img src="images/product.jpg" width="195px" alt=""/></a>
+					<h3>Product Title Here</h3>
+					<span class="price">$33</span>
+				</li>
+				<li><a href="#"><img src="images/product.jpg" width="195px" alt=""/></a>
+					<h3>Product Title Here</h3>
+					<span class="price">$33</span>
+				</li>
+				<li><a href="#"><img src="images/product.jpg" width="195px" alt=""/></a>
+					<h3>Product Title Here</h3>
+					<span class="price">$33</span>
+				</li>
+				<li><a href="#"><img src="images/product.jpg" width="195px" alt=""/></a>
+					<h3>Product Title Here</h3>
+					<span class="price">$33</span>
+				</li>
+				<li><a href="#"><img src="images/product.jpg" width="195px" alt=""/></a>
+					<h3>Product Title Here</h3>
+					<span class="price">$33</span>
+				</li>
+				<li><a href="#"><img src="images/product.jpg" width="195px" alt=""/></a>
+					<h3>Product Title Here</h3>
+					<span class="price">$33</span>
+				</li>
+			</ul>
+
+			</div>
+		</div>
+	</div>
+</div>
+<div class="clear"></div>
+
+<?php include('footer.php')?>
---
+++ product.php
@@ -0,0 +1,40 @@
+<?php include('header.php')?>
+<div class="promobanner row">
+		<div class="column16">
+			<div class="title">FREE SHIPPING ON ALL ORDERS OVER $100</div>
+		</div>
+	</div><!-- end of promo banner -->
+<div class="clear"></div>
+<div class="product-pages">
+	<div class="column6 prod-images omega">
+		<img src="images/product.jpg" alt="Main Product"/>
+		<ul id="subimages">
+			<li><a href="#"><img src="images/product.jpg" alt="image" width="80px"/></a></li>
+			<li><a href="#"><img src="images/product.jpg" alt="image" width="80px"/></a></li>
+			<li><a href="#"><img src="images/product.jpg" alt="image" width="80px"/></a></li>
+			<li><a href="#"><img src="images/product.jpg" alt="image" width="80px"/></a></li>
+		</ul>
+	</div><!-- end of product images -->
+	<div class="column8 prod-info">
+		<h1>Product Title Goes Here</h1>
+		<div class="lgprice">$33</div>
+		<div class="summary">
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin mollis, nunc in semper ultricies, risus mauris tristique dui, quis pretium orci diam malesuada justo. Maecenas blandit sem tortor, vel placerat velit vulputate quis. Nulla sit amet elit vel justo mollis consectetur. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin mollis, nunc in semper ultricies, risus mauris tristique dui, quis pretium orci diam malesuada justo. </p>
+
+		<button>Add to cart</button>
+
+		</div>
+	</div><!-- end of product info -->
+	<div class="column2 prod-crossell alpha">
+		<ul id="crossell">
+		<li><a href="#"><img src="images/product.jpg" alt="crossell" width="80px;"/></a></li>
+		<li><a href="#"><img src="images/product.jpg" alt="crossell" width="80px;"/></a></li>
+		<li><a href="#"><img src="images/product.jpg" alt="crossell" width="80px;"/></a></li>
+		</ul>
+	</div><!-- end of product crossell area -->
+<div class="clear"></div>
+	<div class="column16 prod-upsell"></div><!-- end of product upsell area -->
+</div>
+
+<div class="clear"></div>
+<?php include('footer.php')?>
