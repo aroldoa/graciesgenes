029ca8899c61669533d4bb198ce5f7f61c618a15
Aroldo Alvarez <aroldo@primomotif.com>
2014-02-24 13:46:20 -0600

    Added WordPress Install

--- index.php
+++ index.php
@@ -1,64 +1,17 @@
-<?php include('header.php')?>
-	<div class="slideshow row">
-		<div class="column16">
-			<img src="images/slide.jpg" alt="Slideshow"/>
-		</div>
-	</div><!-- end of slideshow area -->
-	<div class="promobanner row">
-		<div class="column16">
-			<div class="title">FREE SHIPPING ON ALL ORDERS OVER $100</div>
-		</div>
-	</div><!-- end of promo banner -->
-	<div class="products-slider row">
-		<div class="column16">
-			<h2><span>Featured Products</span></h2>
-			<ul class="products">
-				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
-				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
-				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
-				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
-				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
-				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
-				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
-				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
-				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
-				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
-				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
-				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
-				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
-				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
-				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
-			</ul>
-		</div>
-	</div><!-- end of featured products -->
-	<div class="products-slider row">
-		<div class="column16">
-			<h2><span>Sale Items</span></h2>
-			<ul class="products">
-				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
-				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
-				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
-				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
-				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
-				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
-				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
-				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
-				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
-				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
-				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
-				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
-				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
-				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
-				<li><a href="#"><img src="images/product.jpg" width="190px" alt=""/></a></li>
-			</ul>
-		</div>
-	</div><!-- end of best sellers -->
-	<div class="promoarea row">
-		<div class="column8">
-			<img src="images/promo1.jpg" alt="promo1"/>
-		</div>
-		<div class="column8">
-			<img src="images/promo2.jpg" alt="promo2"/>
-		</div>
-	</div><!-- end of promo area -->
-<?php include('footer.php')?>
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
+
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
