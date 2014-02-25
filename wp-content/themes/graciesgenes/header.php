08d6c3bebe8a9b3b5c13d36e7f5452d36828d05d
noncasus <noncasus@gmail.com>
2014-02-25 00:26:36 -0600

    Menu Fix
    Arrow now appears when there are submenus

--- wp-content/themes/graciesgenes/header.php
+++ wp-content/themes/graciesgenes/header.php
@@ -95,7 +95,27 @@
 	
 		<?php wp_nav_menu( $defaults );?>
 		
-	
+	<?php
+	add_filter( 'wp_nav_menu_objects', 'add_menu_parent_class' );
+	function add_menu_parent_class( $items ) {
+		
+		$parents = array();
+		foreach ( $items as $item ) {
+			if ( $item->menu_item_parent && $item->menu_item_parent > 0 ) {
+				$parents[] = $item->menu_item_parent;
+			}
+		}
+		
+		foreach ( $items as $item ) {
+			if ( in_array( $item->ID, $parents ) ) {
+				$item->classes[] = 'menu-parent-item'; 
+			}
+		}
+		
+		return $items;    
+	}
+
+	?>
 
 				
 	<!-- <nav class="row">
