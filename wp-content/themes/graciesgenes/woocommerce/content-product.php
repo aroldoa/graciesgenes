70453817e75a3ddc089dc32fce5b44a0186b5855
noncasus <noncasus@gmail.com>
2014-02-27 14:22:55 -0600

    missing category file

--- wp-content/themes/graciesgenes/woocommerce/content-product.php
+++ wp-content/themes/graciesgenes/woocommerce/content-product.php
@@ -48,7 +48,8 @@ if ( 0 == $woocommerce_loop['loop'] % $woocommerce_loop['columns'] )
 			 * @hooked woocommerce_show_product_loop_sale_flash - 10
 			 * @hooked woocommerce_template_loop_product_thumbnail - 10
 			 */
-			do_action( 'woocommerce_before_shop_loop_item_title' );
+			// do_action( 'woocommerce_before_shop_loop_item_title' );
+			do_action( 'woo_custom_product_category' );
 		?>
 
 		<h3><?php the_title(); ?></h3>
