6a240a9862d06bbf1ecc09cdda04181b7e78d6b5
noncasus <noncasus@gmail.com>
2014-02-28 01:12:43 -0600

    Here we go again, functions and zoom reinstated
    Had to manually copy these, hope this commit works.

--- wp-content/themes/graciesgenes/functions.php
+++ wp-content/themes/graciesgenes/functions.php
@@ -1,17 +1,17 @@
 <?php
 
-	// Add RSS links to <head> section
-	add_theme_support( 'automatic-feed-links' );
+    // Add RSS links to <head> section
+    add_theme_support( 'automatic-feed-links' );
 
-	// Load jQuery
-	if ( !is_admin() ) {
-	   wp_deregister_script('jquery');
-	   wp_register_script('jquery', ("http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"), false);
-	   wp_enqueue_script('jquery');
-	}
+    // Load jQuery
+    if ( !is_admin() ) {
+       wp_deregister_script('jquery');
+       wp_register_script('jquery', ("http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"), false);
+       wp_enqueue_script('jquery');
+    }
 
 
-	//Reposition WooCommerce breadcrumb
+    //Reposition WooCommerce breadcrumb 
     function woocommerce_remove_breadcrumb(){
         remove_action( 'woocommerce_before_main_content', 'woocommerce_breadcrumb', 20);
     }
@@ -32,12 +32,8 @@
 
     add_action( 'woo_custom_breadcrumb', 'woocommerce_custom_breadcrumb' );
 
-    remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10);
 
-	add_action('woo_product_tabs', 'woocommerce_output_product_data_tabs');
-
-
-
+        
     //Two images per item in category
     function woocommerce_remove_product_thumbnail(){
         remove_action('woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_thumbnail', 10 );
@@ -50,24 +46,35 @@
         global $product;
 
         $size = 'shop_catalog';
+        $opening_container = '<div class="overlay">';
         $attachment_ids = $product->get_gallery_attachment_ids();
+        $src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), $size);
+
+         if(has_post_thumbnail() && ($attachment_ids)){
+            $attrs = array(
+                'id'    => 'product_category_image_0',
+                'src'   => $src[0],
+                'class' => "attachment-$size",
+                'alt'   => trim(strip_tags( $attachment->post_excerpt )),
+                'title' => trim(strip_tags( $attachment->post_title )),
+            );
+            $img_tags = (get_the_post_thumbnail( $post->ID, $size, $attrs));
+        }
 
         if($attachment_ids){
-            $opening_container = '<div class="overlay">';
-
-            for($i=0; $i<=1;$i++){
+            for($i=0; $i<=0;$i++){
                 $image_link = wp_get_attachment_url( $attachment_ids[$i] );
                 $image_metadata = wp_get_attachment_metadata( $attachment_ids[$i]);
                 if(isset($image_metadata['sizes'])){
                     $image_size = $image_metadata['sizes'][$size];
                 }
-
+                
                 $img_tags .= '<img id="product_category_image_'. ($i+1) .'" src="' . $image_link . '" width="'. $image_size['width'] .'" height="'. $image_size['height'] .'"  />';
-
             }
             $closing_container = '</div>';
             $images = $opening_container . $img_tags . $closing_container;
             echo($images);
+
         }elseif(has_post_thumbnail()){
             echo (get_the_post_thumbnail( $post->ID, $size ));
         }
@@ -77,22 +84,22 @@
 
     add_action( 'woo_custom_product_category', 'woocommerce_custom_product_category' );
 
+    
 
-
-	// Clean up the <head>
-	function removeHeadLinks() {
-    	remove_action('wp_head', 'rsd_link');
-    	remove_action('wp_head', 'wlwmanifest_link');
+    // Clean up the <head>
+    function removeHeadLinks() {
+        remove_action('wp_head', 'rsd_link');
+        remove_action('wp_head', 'wlwmanifest_link');
     }
     add_action('init', 'removeHeadLinks');
     remove_action('wp_head', 'wp_generator');
 
     register_nav_menus( array(
-    	'primary' => 'Primary Navigation',
-    	'pages' => 'Pages Navigation',
-		'footer' => 'Footer Navigation'
+        'primary' => 'Primary Navigation',
+        'pages' => 'Pages Navigation',
+        'footer' => 'Footer Navigation'
 
-    	) );
+        ) );
 
     add_theme_support('post-thumbnails');
 
@@ -100,74 +107,74 @@
 
     function promotions()
     {
-    	register_post_type('promotions',
-    		array(
-    			'labels' => array(
-    				'name' => __('Promotions'),
-    				'singular_name' => __('Promotion'),
-    				'all_items' => __('All Promotions'),
-    				'add_new' => __('Add New Promotion'),
-    				'add_new_item' => __('Add A New Promotion'),
-    				'new_item' => __('Add Promotion'),
-    				'edit_item' => __('Edit Promotion'),
-    			),
-    			'description' => 'Use this panel to add images to the site Promotions',
-    			'public' => true,
-    			'exclude_from_search' => true,
-    			'menu_position' => 26,
-    			'supports' => array(
-    				'title',
-    				'editor',
-    				'custom-fields',
-    				'excerpt',
-    				'thumbnail',
-    				'page-attributes'
-    			),
-    			// 'hierarchical' => true,
-    			'show_in_nav_menus' => false,
-    		)
-    	);
+        register_post_type('promotions',
+            array(
+                'labels' => array(
+                    'name' => __('Promotions'),
+                    'singular_name' => __('Promotion'),
+                    'all_items' => __('All Promotions'),
+                    'add_new' => __('Add New Promotion'),
+                    'add_new_item' => __('Add A New Promotion'),
+                    'new_item' => __('Add Promotion'),
+                    'edit_item' => __('Edit Promotion'),
+                ),
+                'description' => 'Use this panel to add images to the site Promotions',
+                'public' => true,
+                'exclude_from_search' => true,
+                'menu_position' => 26,
+                'supports' => array(
+                    'title',
+                    'editor',
+                    'custom-fields',
+                    'excerpt',
+                    'thumbnail',
+                    'page-attributes'
+                ),
+                // 'hierarchical' => true,
+                'show_in_nav_menus' => false,
+            )
+        );
     }
 
 
     if (function_exists('register_sidebar')) {
-    	register_sidebar(array(
-    		'name' => 'Sidebar Widgets',
-    		'id'   => 'sidebar-widgets',
-    		'description'   => 'These are widgets for the sidebar.',
-    		'before_widget' => '<div id="%1$s" class="widget %2$s">',
-    		'after_widget'  => '</div>',
-    		'before_title'  => '<h2>',
-    		'after_title'   => '</h2>'
-    	));
+        register_sidebar(array(
+            'name' => 'Sidebar Widgets',
+            'id'   => 'sidebar-widgets',
+            'description'   => 'These are widgets for the sidebar.',
+            'before_widget' => '<div id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h2>',
+            'after_title'   => '</h2>'
+        ));
     }
 
 
   if (function_exists('register_sidebar')) {
-    	register_sidebar(array(
-    		'name' => 'Page Sidebar',
-    		'id'   => 'page-sidebar',
-    		'description'   => 'These are widgets for the sidebar.',
-    		'before_widget' => '<div id="%1$s" class="widget %2$s">',
-    		'after_widget'  => '</div>',
-    		'before_title'  => '<h2>',
-    		'after_title'   => '</h2>'
-    	));
+        register_sidebar(array(
+            'name' => 'Page Sidebar',
+            'id'   => 'page-sidebar',
+            'description'   => 'These are widgets for the sidebar.',
+            'before_widget' => '<div id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h2>',
+            'after_title'   => '</h2>'
+        ));
     }
 
      if (function_exists('register_sidebar')) {
-    	register_sidebar(array(
-    		'name' => 'Footer Sidebar',
-    		'id'   => 'footer-sidebar',
-    		'description'   => 'These are widgets for the sidebar.',
-    		'before_widget' => '<div id="%1$s" class="widget %2$s">',
-    		'after_widget'  => '</div>',
-    		'before_title'  => '<h5>',
-    		'after_title'   => '</h5>'
-    	));
+        register_sidebar(array(
+            'name' => 'Footer Sidebar',
+            'id'   => 'footer-sidebar',
+            'description'   => 'These are widgets for the sidebar.',
+            'before_widget' => '<div id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h5>',
+            'after_title'   => '</h5>'
+        ));
     }
 
     //breadcrumb
-
+   
 
 ?>
--- wp-content/themes/graciesgenes/woocommerce/single-product/product-image.php
+++ wp-content/themes/graciesgenes/woocommerce/single-product/product-image.php
@@ -20,8 +20,25 @@ global $post, $woocommerce, $product;
 			$image_title 		= esc_attr( get_the_title( get_post_thumbnail_id() ) );
 			$image_link  		= wp_get_attachment_url( get_post_thumbnail_id() );
 			$image       		= get_the_post_thumbnail( $post->ID, apply_filters( 'single_product_large_thumbnail_size', 'shop_single' ), array(
-				'title' => $image_title
+				'title' => $image_title,
+				'id'	=> 'zoom'
 				) );
+
+			//first we need to get the url of the image
+			$image_url = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'shop_single');
+
+			//we need to remove the default size in order to replace with larger image for zoom
+			$large_image = explode("338x504", $image_url[0]);
+			$large_image_url = $large_image[0] . '716x1024' . $large_image[1];
+
+			//we need to add the data attribute that the jquery plugin needs for zoom
+			$image_tag = explode("<img",$image);
+			$image_tag_data_attr = 'data-zoom-image="'. $large_image_url .'" ';
+
+			//put everything back together ($image_tag[1] contains the rest of the img tag)
+			$image = '<img ' . $image_tag_data_attr . $image_tag[1];
+
+			
 			$attachment_count   = count( $product->get_gallery_attachment_ids() );
 
 			if ( $attachment_count > 0 ) {
@@ -30,14 +47,14 @@ global $post, $woocommerce, $product;
 				$gallery = '';
 			}
 
-			echo apply_filters( 'woocommerce_single_product_image_html', sprintf( '<a href="%s" itemprop="image" class="woocommerce-main-image zoom" title="%s" data-rel="prettyPhoto' . $gallery . '">%s</a>', $image_link, $image_title, $image ), $post->ID );
+			echo apply_filters( 'woocommerce_single_product_image_html', sprintf( '<a href="%s" itemprop="image" class="woocommerce-main-image zoom" title="%s" data-rel="prettyPhoto' . $gallery . '">%s</a>', $image_link, $image_title, $image), $post->ID );
 
 		} else {
 
 			echo apply_filters( 'woocommerce_single_product_image_html', sprintf( '<img src="%s" alt="Placeholder" />', wc_placeholder_img_src() ), $post->ID );
 
 		}
-	?>
+?>
 
 	<?php do_action( 'woocommerce_product_thumbnails' ); ?>
 
